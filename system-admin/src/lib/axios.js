cb3ff24adaa295a160b886d125dac1cf28631692
Emdi Gemini <yougotjinx@gmail.com>
2026-04-17 12:44:04 +0800

    this is it

--- system-admin/src/lib/axios.js
+++ system-admin/src/lib/axios.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
 
 export const apiAccount = axios.create({
-  baseURL: `${BASE_URL}/api/accounts`
+  baseURL: `${BASE_URL}/accounts`
 });
 
 export const apiStudent = axios.create({
-  baseURL: `${BASE_URL}/api/students`,
+  baseURL: `${BASE_URL}/students`,
   withCredentials: true 
 });
 
 export const apiClass = axios.create({
-  baseURL: `${BASE_URL}/api/classes`
+  baseURL: `${BASE_URL}/classes`
 });
 
 export const apiAdmin = axios.create({
-  baseURL: `${BASE_URL}/api/admins`,
+  baseURL: `${BASE_URL}/admins`,
   withCredentials: true
 });
--- system-client/src/lib/axios.js
+++ system-client/src/lib/axios.js
@@ -1,43 +1,43 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
 
 export const apiAccount = axios.create({
-  baseURL: `${BASE_URL}/api/accounts`,
+  baseURL: `${BASE_URL}/accounts`,
   withCredentials: true 
 });
 
 export const apiUser = axios.create({
-  baseURL: `${BASE_URL}/api/user`,
+  baseURL: `${BASE_URL}/user`,
   withCredentials: true 
 });
 
 export const apiStudent = axios.create({
-  baseURL: `${BASE_URL}/api/students`,
+  baseURL: `${BASE_URL}/students`,
   withCredentials: true 
 });
 
 export const apiTeacher = axios.create({
-  baseURL: `${BASE_URL}/api/teachers`,
+  baseURL: `${BASE_URL}/teachers`,
   withCredentials: true 
 });
 
 export const apiClass = axios.create({
-  baseURL: `${BASE_URL}/api/classes`,
+  baseURL: `${BASE_URL}/classes`,
   withCredentials: true 
 });
 
 export const apiSubject = axios.create({
-  baseURL: `${BASE_URL}/api/subjects`,
+  baseURL: `${BASE_URL}/subjects`,
   withCredentials: true
 });
 
 export const apiAttendance = axios.create({
-  baseURL: `${BASE_URL}/api/attendance`,
+  baseURL: `${BASE_URL}/attendance`,
   withCredentials: true
 });
 
 export const apiSched = axios.create({
-  baseURL: `${BASE_URL}/api/schedule`,
+  baseURL: `${BASE_URL}/schedule`,
   withCredentials: true
 });
