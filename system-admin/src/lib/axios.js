5f7fd604547497b5585d157195a863809fce96a1
Emdi Gemini <yougotjinx@gmail.com>
2026-04-17 13:13:14 +0800

    attendance system v1

--- system-admin/src/lib/axios.js
+++ system-admin/src/lib/axios.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
 
-console.log(BASE_URL);
-
 export const apiAccount = axios.create({
   baseURL: `${BASE_URL}/accounts`
 });
